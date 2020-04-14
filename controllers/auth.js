6734caa2b4fb9887af45cceda4694ba66a03e267
oluwadareseyi <oluwadareseyii@gmail.com>
2020-04-14 19:33:06 +0100

    add validation for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,4 +2,6 @@ const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const errorHandler = require("../util/error");
 
-exports.signUp = (req, res, next) => {};
+exports.signUp = (req, res, next) => {
+  const { email, password, name } = req.body;
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const { body } = require("express-validator");
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/signup", [body("")], authController.signUp);
+router.post(
+  "/signup",
+  [
+    body("email", "Please enter a valid email")
+      .isEmail()
+      .custom(async (value, { req }) => {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          return Promise.reject("E-Mail already exists");
+        }
+      })
+      .normalizeEmail(),
+    body("password", "Password must have a minimum of 5 characters")
+      .trim()
+      .isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signUp
+);
 
 module.exports = router;
