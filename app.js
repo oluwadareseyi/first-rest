48ce94da4700472dbcabd4a2520c4c623dd45c39
oluwadareseyi <oluwadareseyii@gmail.com>
2020-04-14 01:09:36 +0100

    set up image path

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const feedRoutes = require("./routes/feed");
 app.use(cors());
 
 app.use(bodyparser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // app.use(express.json({ limit: "10kb" }));
 // app.use(express.urlencoded({ extended: true }));
