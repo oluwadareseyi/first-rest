f580717ac128dcdcd7b37c059546718abda6aa14
oluwadareseyi <oluwadareseyii@gmail.com>
2020-04-15 19:52:27 +0100

    send token as response

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const errorHandler = require("../util/error");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = async (req, res, next) => {
   const errors = validationResult(req);
@@ -41,6 +42,16 @@ exports.login = async (req, res, next) => {
     if (!isEqual) {
       errorHandler("Please enter a correct password", 401);
     }
+
+    const token = jwt.sign(
+      { email: user.email, userId: user._id.toString() },
+      "secretdonttellanyone",
+      { expiresIn: "1h" }
+    );
+
+    res
+      .status(200)
+      .json({ message: "Successfully logged in", token, userId: user._id });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 500;
